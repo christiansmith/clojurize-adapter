e846e5c4fa6e299ed0da13f70be911a20810fac8
Christian Smith <http://github.com/christiansmith>
2010-11-01 11:47:38 -0700

    Initial commit

---
+++ src/clojurize_adapter/core.clj
@@ -0,0 +1,1 @@
+(ns clojurize-adapter.core)
---
+++ test/clojurize_adapter/test/core.clj
@@ -0,0 +1,6 @@
+(ns clojurize-adapter.test.core
+  (:use [clojurize-adapter.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
