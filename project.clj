46243138df0e2b7bbe3a683b9ab6863fa0657184
Christian Smith <http://github.com/christiansmith>
2010-11-03 13:34:05 -0700

    added clojurize-protocol dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clojurize-adapter "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [clojurize-protocol "1.0.0-SNAPSHOT"]])
